00bc4bab40961ef1ed51ebefc7af30fb56e9e0d7
Tiago Bernardo <hsbtiago@live.com>
2020-12-12 16:26:01 -0400

    Datatable em portugues na lista de designaões

--- App/wwwroot/js/site.js
+++ App/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready( function () {
+    $('.montarDataTable').DataTable({
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json"
+        }
+    });
+} );
