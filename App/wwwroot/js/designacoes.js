320d2a00568c9640d0e9369e757b2ef2e96782f8
Tiago Bernardo <hsbtiago@live.com>
2020-12-18 15:50:07 -0400

    Preparando imagem da designação para compartilhamento

--- App/wwwroot/js/designacoes.js
+++ App/wwwroot/js/designacoes.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let tipoDaDesignacao = document.querySelector('#Tipo').value
-    let checkboxes = document.querySelectorAll('[name=Tipo]')
+    MarcarCheckboxDeAcordoComModel()
+    ConverterHTMlemImagem()
+
+})
+
+function MarcarCheckboxDeAcordoComModel() {
+    let tipoDaDesignacao = document.querySelector('#Tipo').value;
+    let checkboxes = document.querySelectorAll('[name=Tipo]');
 
     checkboxes.forEach(checkbox => {
 
         if (checkbox.value == tipoDaDesignacao) {
-            checkbox.checked = true
+            checkbox.checked = true;
         }
 
     });
-})
+}
+
+function ConverterHTMlemImagem() {
+
+    var $divDesignacao = document.querySelector("#divDesignacao")
+    let $printDesignacao = document.querySelector('#printDesignacao')
+
+    html2canvas($divDesignacao).then(function(canvas) {
+        $divDesignacao.style.display = 'none';
+        $printDesignacao.appendChild(canvas);
+    });
+}
