4eed8ff2e865eb78b77d27482c40c4c3efc24f3e
Tiago Bernardo <hsbtiago@live.com>
2021-02-27 17:51:16 -0400

    Adicionar tipo de designacao Convite para Celebracao

--- App/Constantes/Tipos.cs
+++ App/Constantes/Tipos.cs
@@ -2,6 +2,6 @@ namespace App.Constantes
 {
     public static class Tipos
     {
-        public static string[] TiposDeDesignacao => new[] {"Leitura", "Primeira Conversa", "Revisita", "Estudo Bíblico", "Discurso"};        
+        public static string[] TiposDeDesignacao => new[] {"Convite da Celebração", "Leitura", "Primeira Conversa", "Revisita", "Estudo Bíblico", "Discurso"};        
     }
 }
