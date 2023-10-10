6573a0463ab8bf5c3c020f6db614abe9b1e38f12
Superman <tiago.bernardo@parceiro.aliare.co>
2023-10-10 16:49:21 -0300

    sumir com tipo convite da celebracao

--- App/Constantes/Tipos.cs
+++ App/Constantes/Tipos.cs
@@ -2,6 +2,14 @@ namespace App.Constantes
 {
     public static class Tipos
     {
-        public static string[] TiposDeDesignacao => new[] {"Convite da Celebração", "Leitura", "Primeira Conversa", "Revisita", "Estudo Bíblico", "Discurso"};        
+        public static string[] TiposDeDesignacao => new[]
+        {
+            // "Convite da Celebração",
+            "Leitura",
+            "Primeira Conversa",
+            "Revisita",
+            "Estudo Bíblico",
+            "Discurso"
+         };
     }
 }
