f302e5fc6a630a12dde26a956b8e081e573b8f63
Superman <tiago.bernardo@parceiro.aliare.co>
2023-10-10 22:46:42 -0300

    ajuste na legenda

--- App/Models/ListarPublicador.cs
+++ App/Models/ListarPublicador.cs
@@ -5,7 +5,7 @@ namespace App.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public Designacao UltimaDesignacao { get; set; }        
-        public string DataUltimaDesigacao => UltimaDesignacao != null ? UltimaDesignacao.Data.ToShortDateString(): "Nunca Designado";
-        public string NomeComData => $"{Nome} - Última Designacao em {DataUltimaDesigacao}";
+        public string DataUltimaDesigacao => UltimaDesignacao != null ? $"Última designacao em {UltimaDesignacao.Data.ToShortDateString()}": "Nunca Designado";
+        public string NomeComData => $"{Nome} - {DataUltimaDesigacao}";
     }
 }
