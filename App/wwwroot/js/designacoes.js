23503755e45b913520feeaaaa7a399ae5506283f
Superman <tiago.bernardo@parceiro.aliare.co>
2023-10-26 16:36:58 -0300

    testando novo esquema de imagem

--- App/wwwroot/js/designacoes.js
+++ App/wwwroot/js/designacoes.js
@@ -6,28 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-function OcultarObservacoes() {    
+function OcultarObservacoes() {
     $('.observacao').hide()
 }
 
 function MarcarCheckboxDeAcordoComModel() {
-    
+
     let tipoDaDesignacao = document.querySelector('#Tipo').value;
     let checkboxes = document.querySelectorAll('.check-tipo');
 
     checkboxes.forEach(checkbox => {
 
-        console.log(tipoDaDesignacao)
-
-        if (checkbox.value == tipoDaDesignacao) {            
+        if (checkbox.value == tipoDaDesignacao) {
 
             checkbox.checked = true;
             var $span = checkbox.parentNode.querySelector('.observacao')
-            
+
             if ($span) {
                 $span.style.display = 'block' // exibir linha da observacao                
             }
-            
+
         }
 
     });
@@ -35,11 +33,16 @@ function MarcarCheckboxDeAcordoComModel() {
 
 function ConverterHTMlemImagem() {
 
-    var $divDesignacao = document.querySelector("#divDesignacao")
-    let $printDesignacao = document.querySelector('#printDesignacao')
+    const $divDesignacao = document.querySelector("#divDesignacao")
+    const $printDesignacao = document.querySelector('#printDesignacao')
+    const $img = document.querySelector('#imgDesignacao')
 
     html2canvas($divDesignacao).then(function (canvas) {
+        
         $divDesignacao.style.display = 'none';
         $printDesignacao.appendChild(canvas);
+        $img.src = canvas.toDataURL('image/png')
+
     });
+
 }
